18f87aa3d619ca5393280c63dbb82ecc22364f8b
dreamheart <leondreamheart@outlook.com>
2021-08-19 15:31:20 -0400

    updated module to separate date from exercises

--- models/workout.js
+++ models/workout.js
@@ -3,34 +3,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  exercises: {
-    type: Array,
-  },
-  name: {
-    type: String,
-  },
-  type: {
-    type: String,
-  },
-  weight: {
-    type: Number,
-  },
-  sets: {
-    type: Number,
-  },
-  reps: {
-    type: Number,
-  },
-  duration: {
-    type: Number,
-  },
-  distance: {
-    type: Number,
-  },
   day: {
     type: Date,
     default: Date.now,
   },
+  exercises: {
+    name: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    weight: {
+      type: Number,
+    },
+    sets: {
+      type: Number,
+    },
+    reps: {
+      type: Number,
+    },
+    duration: {
+      type: Number,
+    },
+    distance: {
+      type: Number,
+    }
+  }
 });
 
 // Export Modules
